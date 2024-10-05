61510ed0d9a0cc6c9e6f971a77af842c2a5c645a
Josiah Aideloje <aideloje.josiah@lmu.edu.ng>
2024-10-05 16:48:27 +0100

    Recent changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,44 @@
-import React from "react";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import { fetchLatestInvoices } from "../app/lib/data";
 
-const HomePage = () => {
+export default async function LatestInvoices() {
+  // Fetch the latest invoices
+  const latestInvoices = await fetchLatestInvoices();
+
   return (
-    <div>
-      <h1>Welcome to Next.js!</h1>
-      <p>This is a default page.</p>
+    <div className="flex w-full flex-col md:col-span-4">
+      <h2>Latest Invoices</h2>
+      <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
+        <div className="bg-white px-6">
+          {latestInvoices.map((invoice) => (
+            <div key={invoice.id}>
+              <div className="flex items-center">
+                <Image
+                  src={invoice.image_url}
+                  alt={`${invoice.name}'s profile picture`}
+                  className="mr-4 rounded-full"
+                  width={32}
+                  height={32}
+                />
+                <div className="min-w-0">
+                  <p className="truncate text-sm font-semibold md:text-base">
+                    {invoice.name}
+                  </p>
+                  <p className="hidden text-sm text-gray-500 sm:block">
+                    {invoice.email}
+                  </p>
+                </div>
+              </div>
+              <p>{invoice.amount}</p>
+            </div>
+          ))}
+        </div>
+        <div className="flex items-center pb-2 pt-6">
+          <ArrowPathIcon className="h-5 w-5 text-gray-500" />
+          <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
+        </div>
+      </div>
     </div>
   );
-};
-
-export default HomePage;
+}
